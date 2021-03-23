82f922aa0fa9b87bbfa4417661e9743bf8ec14a3
Mike Wilcox <anm9tr@gmail.com>
2021-03-23 14:56:14 -0500

    publish

---
+++ build/README.md
@@ -0,0 +1,308 @@
+# dom
+
+A convenience library for common DOM methods.
+
+dom uses UMD, so it will work with globals, AMD, or Node-style module exports.
+
+## Getting Started
+
+To install using bower:
+
+	bower install @clubajax/dom --save
+	
+Or npm:
+
+    npm install @clubajax/dom --save
+    
+You can clone the repository with your generic clone commands as a standalone 
+repository or submodule.
+
+	git clone git://github.com/clubajax/dom.git
+
+With AMD, it is recommended that you set the config.path of RequireJS to make `dom` accessible as an
+absolute path.
+	
+`dom` has no dependencies.
+
+## Description
+
+The primary function is to make it more finger-friendly to create, modify, and delete DOM nodes, and to add and remove 
+styles and attributes. There is some extended functionality as noted in the API docs below.
+
+## Support
+
+`dom` supports modern browsers, IE9 and up. Some modern DOM methods like `classList` are expected.
+
+This library uses UMD, meaning it can be consumed with RequireJS, Browserify (CommonJS),
+or a standard browser global.
+
+## The `dom` function
+
+`dom` is the main function, and has several more functions attached to that function. So you can
+create a node with the main function like:
+
+```jsx harmony
+dom('div');
+```
+	
+And you could access nodes with the `byId` attached function, like:
+
+```jsx harmony
+var node = dom.byId('my-node');
+```
+
+## API
+
+**dom()**
+
+Creates and returns a node.
+
+
+if the first argument is a string and starts with '<', it is assumed to use be an HTML string, and
+creates a node using innerHTML. Optional second arg is a parentNode.
+```javascript
+var node = dom('<div>my dome node</div>', parentNode);
+```
+
+The previous can be more explicitly done by using `dom.toDom()`
+
+if the first argument is a string and does *not* start with '<', it is assumed to be a nodeName,
+and a node is created via `document.createElement()`.
+```jsx harmony
+var node = dom('div');
+```
+Additional parameters (all optional):
+
+The second parameter is `options` which is an object that can contain properties or other objects:
+
+* `id`: sets the node ID
+* `class` or `className`: Sets the node class
+* `html` or `innerHTML`: Sets the content of the node, Possible values:
+  * string: will be added as innerHTML
+  * object (HtmlElement): Will be be added via appendChild
+  * array: HtmlElements will be appended and strings will be converted into dom nodes and appended
+* `style`: An object of CSS key-value styles. This object is passed to dom.style()
+* All other keys are expected to be attributes.
+  * (The `attr` object has been deprecated from 1.x)
+	
+The third parameter is an node or a node id, where the newly created node will be appended
+
+The fourth parameter is a boolean. If true, the newly created node will be prepended, not appended.
+	
+The `html` can accept 
+
+**dom.fromDom()**
+Converts a dom node and its children into a JavaScript object. Example:
+```html
+<div id="option-div" class="foo" value="bar" selected="a" disabled="true" is-prop="">
+    <option value="a">A</option>
+    <option value="b" selected="">B</option>
+    <option value="c">C</option>
+</div>
+```
+```jsx harmony
+result = {
+    children:[{
+        text: 'A',
+           value: 'a'
+    },{
+        text: 'A',
+        value: 'b'
+    },{
+        text: 'C',
+        value: 'c'
+    }],
+    class:"foo",
+    disabled:true,
+    id:"option-div",
+    'is-prop':true,
+    selected:"a",
+    text:"",
+    value:"bar"
+}
+```
+**dom.style()**
+        
+`dom.style` is a getter or a setter, depending on the parameters passed.
+
+To use as a getter, the parameters should be a node, and a string property (only one property can be
+accessed at a time). If the result is in the node.style object, that is returned. If not, the
+property is acquired through the window global `getComputedStyle`.
+
+To use as a setter, add a third parameter as a value:
+```jsx harmony
+dom.style(node, 'width', 100);
+```
+Note that like jQuery, the value did not need to be a string appended with 'px'. If the style is a
+dimensional property, this is done automatically. The dimensional properties are:
+
+	width
+	height
+	top
+	left
+	right
+	bottom
+	maxWidth
+	max-width
+	maxHeight
+	max-height
+
+A more common way to use as a setter is to make the second parameter an object:
+```jsx harmony
+dom.style(node, {
+    width: 100,
+    height: '10em',
+    top: '50%',
+    position: 'absolute',
+    zIndex: 1
+});
+```
+**dom.attr()**
+
+Similar to `dom.style`, `dom.attr` is a getter/setter to get and set attributes on nodes.
+
+As a getter:
+
+```jsx harmony
+var dataItem = dom.attr(node, 'data-item');
+```
+
+As a setter:
+
+```jsx harmony
+dom.attr(node, 'data-item', dataItem);
+```
+
+Or multiple attributes:
+```jsx harmony
+dom.attr(node, {
+    'data-item': dataItem,
+    scrollTop: 100,
+    contentEditable: true
+});
+```
+
+Complex objects can be passed while creating custom-element nodes, via `attr` as a convenience:
+```jsx harmony
+dom('my-custom', {
+    myObject:{
+        a:1,
+        bool:tru
+    }
+});
+```
+You could also use dom.attr() to pass a node, but that wouldn't make much sense.
+
+Events can be added to a node. Is is expected that the value will be a function and the key will be an event name, prepended with `on` and a capital, such as in the example:
+```jsx harmony
+dom('button', {
+    onClick: function () {
+    	console.log('The button has been clicked');
+    }
+});
+```
+
+A MutationObserver is used to disconnect the event when the node is destroyed or removed from the document.
+
+If the key is not appended with `on`, it is treated as an object and assigned to the node (which can then be called as a method).
+
+**dom.byId('nodeId')**
+
+If the parameter is a string, it finds a node with document.getElementByid(). If not found, returns
+null. If the parameter is an object, it is assumed to already be a node and is returned.
+
+Bonus: if it is detected to be a jQuery $element, it "de-jQuery-ifies" it, and returns a node.
+
+**dom.box(node)**
+
+Returns the dimensions of the passed node. Mainly an alias for `getBoundingClientRect()`. If the
+passed item in the window object, returns its width and height.
+
+**dom.relBox(node, ?parent)**
+
+Returns the dimensions of the passed node and it's position relative to its parent node. Returns simple `{x,y,w,h}`. Assumes to compare position to immediate parent unless second node is passed.
+
+**dom.size(node, type)**
+
+Returns the width and height of the passed node. If the position is not needed, this is faster than `dom.box`. The `type` parameter indicates:
+ * `client`: returns the inner height/width of an element in pixels, including padding but not the scrollbar height, border, or margin.
+ * `scroll`: is a measurement of the height/width of an element's content including content not visible on the screen due to overflow.
+ * `offset` (default): is a measurement which includes the element borders, the element padding, the element scrollbar (if present, if rendered) and the element CSS height/width.
+
+**dom.query([?node], 'div.myClass')**
+
+An alias for `document.querySelector`, so the parameter should conform to that.
+
+**dom.queryAll([?node]'div.myClass')**
+
+An alias for `document.querySelectorAll`, so the parameter should conform to that. Returns an Array, not a
+NodeList.
+
+**dom.insertAfter(refNode, node)**
+
+Inserts a node after the reference sibling.
+
+**dom.place(parentNode, node, position)**
+
+Inserts a node at the child position of the parentNode. Handles bad positions (null, > children.length, etc).
+
+**dom.destroy(node)**
+
+Destroys a node completely.
+
+**dom.clean(node)**
+
+Removes (but does not destroy) all child nodes.
+
+**dom.classList**
+
+`dom.classList` is essentially a passthrough for node.classList methods, `add`, `remove`, `toggle`, and
+`contains`.
+
+There is extended functionality in `add` and `remove`: standard functionality doesn't allow for Arrays or
+strings with spaces (ergo, two classes at once). `dom.classList` allows for this.
+
+`toggle` works around an IE bug.
+
+Examples:
+```jsx harmony
+dom.classList.toggle(node, 'selected', !!this.isSelected);
+dom.classList.add(node, 'foo bar baz');
+dom.classList.remove(node, 'foo baz');
+```
+
+**dom.normalize()**
+
+Returns a normalized value from the passed string. Conversions look like:
+
+    'true' => true
+    'false' => false
+    'null' => null
+    '1.5' => 1.5
+
+**dom.frag()**
+
+Pass in an array of nodes and it returns a document fragment:
+
+    var frag = dom.frag([
+        dom('header', { html: `Custom ${item.label}` }),
+        dom('h1', { html: `Value: ${item.value}` })
+    ]);
+    node.appendChild(frag);
+    
+## Comes with Stub Included!
+
+The benefit of using `dom` exclusively in your code, is it comes complete with a "dom stub",
+which can be substituted in Node.js unit tests.
+
+The stub is as described, a stub, meaning most of the functions are NOOPs. But there are a few
+functions that work as mocks, meaning they are functional with dummy/partial data. Simple objects
+are returned as dom nodes, which should result to truthy in tests. These simple objects have an
+`innerHTML` getter/setter so text comparisons can also be done. They also keep track of their
+children and parent nodes, so some DOM manipulation can be simulated.
+
+It is recommended that the stub/dom.js should be looked over to learn of its full functionality.
+
+## License
+
+This uses the [MIT license](./LICENSE). Feel free to use, and redistribute at will.
---
+++ build/dom.js
@@ -0,0 +1,561 @@
+/* UMD.define */
+(function (root, factory) {
+	if (typeof customLoader === 'function') {
+		customLoader(factory, 'dom');
+	} else if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	} else {
+		root.returnExports = factory();
+		window.dom = factory();
+	}
+}(this, function () {
+	'use strict';
+	var
+		uids = {},
+		destroyer = document.createElement('div');
+
+	function isDimension (prop) {
+		return !/opacity|index|flex|weight|^sdcsdcorder|tab|miter|group|zoom/i.test(prop)
+	}
+
+	function isNumber (value) {
+		if (/\s/.test(value)) {
+			return false;
+		}
+		return !isNaN(parseFloat(value));
+	}
+
+	function uid (type) {
+		type = type || 'uid';
+		if (uids[type] === undefined) {
+			uids[type] = 0;
+		}
+		var id = type + '-' + (uids[type] + 1);
+		uids[type]++;
+		return id;
+	}
+
+	function isNode (item) {
+		// safer test for custom elements in FF (with wc shim)
+		// fragment is a special case
+		return !!item && typeof item === 'object' && (typeof item.innerHTML === 'string' || item.nodeName === '#document-fragment');
+	}
+
+	function byId (item) {
+		if (typeof item === 'string') {
+			return document.getElementById(item);
+		}
+		return item;
+	}
+
+	function style (node, prop, value) {
+		var computed, result;
+		if (typeof prop === 'object') {
+			// object setter
+			Object.keys(prop).forEach(function (key) {
+				style(node, key, prop[key]);
+			});
+			return null;
+		} else if (value !== undefined) {
+			// property setter
+			if (typeof value === 'number' && isDimension(prop)) {
+				value += 'px';
+			}
+			node.style[prop] = value;
+		}
+
+		// getter, if a simple style
+		if (node.style[prop]) {
+			result = node.style[prop];
+			if (/px/.test(result)) {
+				return parseFloat(result);
+			}
+			if (/%/.test(result)) {
+				return parseFloat(result) * 0.01;
+			}
+			if (isNumber(result)) {
+				return parseFloat(result);
+			}
+			return result;
+		}
+
+		// getter, computed
+		computed = window.getComputedStyle(node);
+		if (computed[prop]) {
+			result = computed[prop];
+			if (isNumber(result)) {
+				return parseFloat(result);
+			}
+			return computed[prop];
+		}
+		return '';
+	}
+
+	function attr (node, prop, value) {
+		if (typeof prop === 'object') {
+
+			var bools = {};
+			var strings = {};
+			var objects = {};
+			var events = {};
+			var functions = {};
+			Object.keys(prop).forEach(function (key) {
+				if (typeof prop[key] === 'boolean') {
+					bools[key] = prop[key];
+				} else if (typeof prop[key] === 'object') {
+					objects[key] = prop[key];
+				} else if (typeof prop[key] === 'function') {
+					if (/on[A-Z]/.test(key)) {
+						events[key] = prop[key];
+					} else {
+						functions[key] = prop[key];
+					}
+				} else {
+					strings[key] = prop[key];
+				}
+			});
+
+			// assigning properties in specific order of type, namely objects last
+			Object.keys(bools).forEach(function (key) { attr(node, key, prop[key]); });
+			Object.keys(strings).forEach(function (key) { attr(node, key, prop[key]); });
+			Object.keys(events).forEach(function (key) { attr(node, key, prop[key]); });
+			Object.keys(objects).forEach(function (key) { attr(node, key, prop[key]); });
+			Object.keys(functions).forEach(function (key) { attr(node, key, prop[key]); });
+
+			return null;
+		}
+		else if (value !== undefined) {
+			if (prop === 'text' || prop === 'html' || prop === 'innerHTML') {
+				// ignore, handled during creation
+				return;
+			}
+			else if (prop === 'className' || prop === 'class') {
+				dom.classList.add(node, value);
+			}
+			else if (prop === 'style') {
+				style(node, value);
+			}
+			else if (prop === 'attr') {
+				// back compat
+				attr(node, value);
+			}
+			else if (typeof value === 'function') {
+				if (/on[A-Z]/.test(prop)) {
+					attachEvent(node, prop, value);
+				} else {
+					node[prop] = value;
+				}
+			}
+			else if (typeof value === 'object') {
+				// object, like 'data'
+				node[prop] = value;
+			}
+			else {
+				if (value === false) {
+					node.removeAttribute(prop);
+				} else {
+					node.setAttribute(prop, value);
+				}
+			}
+		}
+
+		return node.getAttribute(prop);
+	}
+
+	function attachEvent (node, prop, value) {
+		var event = toEventName(prop);
+		node.addEventListener(event, value);
+
+		var callback = function (mutationsList) {
+			mutationsList.forEach(function (mutation) {
+				for (var i = 0; i < mutation.removedNodes.length; i++) {
+					var n = mutation.removedNodes[i];
+					if (n === node) {
+						node.removeEventListener(event, value);
+						observer.disconnect();
+						break;
+					}
+				}
+			});
+		};
+		var observer = new MutationObserver(callback);
+		observer.observe(node.parentNode || document.body, { childList: true });
+	}
+
+	function box (node) {
+		if (node === window) {
+			node = document.documentElement;
+		}
+		// node dimensions
+		// returned object is immutable
+		// add scroll positioning and convenience abbreviations
+		var
+			dimensions = byId(node).getBoundingClientRect();
+		return {
+			top: dimensions.top,
+			right: dimensions.right,
+			bottom: dimensions.bottom,
+			left: dimensions.left,
+			height: dimensions.height,
+			h: dimensions.height,
+			width: dimensions.width,
+			w: dimensions.width,
+			scrollY: window.scrollY,
+			scrollX: window.scrollX,
+			x: dimensions.left + window.pageXOffset,
+			y: dimensions.top + window.pageYOffset
+		};
+	}
+
+	function relBox (node, parentNode) {
+		var parent = parentNode || node.parentNode;
+		var pBox = box(parent);
+		var bx = box(node);
+
+		return {
+			w: bx.w,
+			h: bx.h,
+			x: bx.left - pBox.left,
+			y: bx.top - pBox.top
+		};
+	}
+
+	function size (node, type) {
+		if (node === window) {
+			node = document.documentElement;
+		}
+		if (type === 'scroll') {
+			return {
+				w: node.scrollWidth,
+				h: node.scrollHeight
+			};
+		}
+		if (type === 'client') {
+			return {
+				w: node.clientWidth,
+				h: node.clientHeight
+			};
+		}
+		return {
+			w: node.offsetWidth,
+			h: node.offsetHeight
+		};
+	}
+
+	function query (node, selector) {
+		if (!selector) {
+			selector = node;
+			node = document;
+		}
+		return node.querySelector(selector);
+	}
+
+	function queryAll (node, selector) {
+		if (!selector) {
+			selector = node;
+			node = document;
+		}
+		var nodes = node.querySelectorAll(selector);
+
+		if (!nodes.length) {
+			return [];
+		}
+
+		// convert to Array and return it
+		return Array.prototype.slice.call(nodes);
+	}
+
+	function toDom (html, options, parent) {
+		var node = dom('div', { html: html });
+		parent = byId(parent || options);
+		if (parent) {
+			while (node.firstChild) {
+				parent.appendChild(node.firstChild);
+			}
+			return node.firstChild;
+		}
+		if (html.indexOf('<') !== 0) {
+			return node;
+		}
+		return node.firstChild;
+	}
+
+	function fromDom (node) {
+		function getAttrs (node) {
+			var att, i, attrs = {};
+			for (i = 0; i < node.attributes.length; i++) {
+				att = node.attributes[i];
+				attrs[att.localName] = normalize(att.value === '' ? true : att.value);
+			}
+			return attrs;
+		}
+
+		function getText (node) {
+			var i, t, text = '';
+			for (i = 0; i < node.childNodes.length; i++) {
+				t = node.childNodes[i];
+				if (t.nodeType === 3 && t.textContent.trim()) {
+					text += t.textContent.trim();
+				}
+			}
+			return text;
+		}
+
+		var i, object = getAttrs(node);
+		object.text = getText(node);
+		object.children = [];
+		if (node.children.length) {
+			for (i = 0; i < node.children.length; i++) {
+				object.children.push(fromDom(node.children[i]));
+			}
+		}
+		return object;
+	}
+
+	function addChildren (node, children) {
+		if (Array.isArray(children)) {
+			for (var i = 0; i < children.length; i++) {
+				if (children[i]) {
+					if (typeof children[i] === 'string') {
+						node.appendChild(toDom(children[i]));
+					} else {
+						node.appendChild(children[i]);
+					}
+				}
+			}
+		}
+		else if (children) {
+			node.appendChild(children);
+		}
+	}
+
+	function removeChildren (node) {
+		var children = [];
+		while (node.children.length) {
+			var child = node.children[0];
+			children.push(node.removeChild(child));
+		}
+		return children;
+	}
+
+	function addContent (node, options) {
+		var html;
+		if (options.html !== undefined || options.innerHTML !== undefined) {
+			html = options.html || options.innerHTML || '';
+			if (typeof html === 'object') {
+				addChildren(node, html);
+			} else {
+				// careful assuming textContent -
+				// misses some HTML, such as entities (&npsp;)
+				node.innerHTML = html;
+			}
+		}
+		if (options.text) {
+			node.appendChild(document.createTextNode(options.text));
+		}
+		if (options.children) {
+			addChildren(node, options.children);
+		}
+	}
+
+	function dom (nodeType, options, parent, prepend) {
+		options = options || {};
+
+		// if first argument is a string and starts with <, pass to toDom()
+		if (nodeType.indexOf('<') === 0) {
+			return toDom(nodeType, options, parent);
+		}
+
+		var node = document.createElement(nodeType);
+
+		parent = byId(parent);
+
+		addContent(node, options);
+
+		attr(node, options);
+
+		if (parent && isNode(parent)) {
+			if (prepend && parent.hasChildNodes()) {
+				parent.insertBefore(node, parent.children[0]);
+			} else {
+				parent.appendChild(node);
+			}
+		}
+
+		return node;
+	}
+
+	function insertAfter (refNode, node) {
+		var sibling = refNode.nextElementSibling;
+		if (!sibling) {
+			refNode.parentNode.appendChild(node);
+		} else {
+			refNode.parentNode.insertBefore(node, sibling);
+		}
+		return sibling;
+	}
+
+	function place (parent, node, position) {
+		if (!parent.children.length ||
+			position === null ||
+			position === undefined ||
+			position === -1 ||
+			position >= parent.children.length
+		) {
+			parent.appendChild(node);
+			return;
+		}
+		parent.insertBefore(node, parent.children[position]);
+	}
+
+	function destroy (node) {
+		// destroys a node completely
+		//
+		if (node) {
+			node.destroyed = true;
+			destroyer.appendChild(node);
+			destroyer.innerHTML = '';
+		}
+	}
+
+	function clean (node, dispose) {
+		//	Removes all child nodes
+		//		dispose: destroy child nodes
+		if (dispose) {
+			while (node.children.length) {
+				destroy(node.children[0]);
+			}
+			return;
+		}
+		while (node.children.length) {
+			node.removeChild(node.children[0]);
+		}
+	}
+
+	dom.frag = function (nodes) {
+		var frag = document.createDocumentFragment();
+		if (arguments.length > 1) {
+			for (var i = 0; i < arguments.length; i++) {
+				frag.appendChild(arguments[i]);
+			}
+		} else {
+			if (Array.isArray(nodes)) {
+				nodes.forEach(function (n) {
+					frag.appendChild(n);
+				});
+			} else {
+				frag.appendChild(nodes);
+			}
+		}
+		return frag;
+	};
+
+	dom.classList = {
+		// in addition to fixing IE11-toggle,
+		// these methods also handle arrays
+		remove: function (node, names) {
+			toArray(names).forEach(function (name) {
+				node.classList.remove(name);
+			});
+		},
+		add: function (node, names) {
+			toArray(names).forEach(function (name) {
+				node.classList.add(name);
+			});
+		},
+		contains: function (node, names) {
+			return toArray(names).every(function (name) {
+				return node.classList.contains(name);
+			});
+		},
+		toggle: function (node, names, value) {
+			names = toArray(names);
+			if (typeof value === 'undefined') {
+				// use standard functionality, supported by IE
+				names.forEach(function (name) {
+					node.classList.toggle(name, value);
+				});
+			}
+			// IE11 does not support the second parameter
+			else if (value) {
+				names.forEach(function (name) {
+					node.classList.add(name);
+				});
+			}
+			else {
+				names.forEach(function (name) {
+					node.classList.remove(name);
+				});
+			}
+		}
+	};
+
+	function normalize (val) {
+		if (typeof val === 'string') {
+			val = val.trim();
+			if (val === 'false') {
+				return false;
+			} else if (val === 'null') {
+				return null;
+			} else if (val === 'true') {
+				return true;
+			}
+			// finds strings that start with numbers, but are not numbers:
+			// '2team' '123 Street', '1-2-3', etc
+			if (('' + val).replace(/-?\d*\.?\d*/, '').length) {
+				return val;
+			}
+		}
+		if (!isNaN(parseFloat(val))) {
+			return parseFloat(val);
+		}
+		return val;
+	}
+
+	function toArray (names) {
+		if (!names) {
+			return [];
+		}
+		return names.split(' ').map(function (name) {
+			return name.trim();
+		}).filter(function (name) {
+			return !!name;
+		});
+	}
+
+	function toEventName (s) {
+		s = s.replace('on', '');
+		var str = '';
+		for (var i = 0; i < s.length; i++) {
+			if (i === 0 || s.charCodeAt(i) > 90) {
+				str += s[i].toLowerCase();
+			} else {
+				str += '-' + s[i].toLowerCase();
+			}
+		}
+		return str;
+	}
+
+	dom.normalize = normalize;
+	dom.clean = clean;
+	dom.query = query;
+	dom.queryAll = queryAll;
+	dom.byId = byId;
+	dom.attr = attr;
+	dom.box = box;
+	dom.style = style;
+	dom.destroy = destroy;
+	dom.uid = uid;
+	dom.isNode = isNode;
+	dom.toDom = toDom;
+	dom.fromDom = fromDom;
+	dom.insertAfter = insertAfter;
+	dom.size = size;
+	dom.relBox = relBox;
+	dom.place = place;
+	dom.removeChildren = removeChildren;
+
+	return dom;
+}));
